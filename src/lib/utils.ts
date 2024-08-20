b1879aded1f5186d59e79cc37d19c55dbaf78de7
khoatruong19 <thpthv.truonghuynhdangkhoa@gmail.com>
2024-08-20 19:13:39 +0700

    fix: unused type

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,7 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 import { format } from "date-fns";
-import { Game, GameStatus } from "./types";
+import { GameStatus } from "./types";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
