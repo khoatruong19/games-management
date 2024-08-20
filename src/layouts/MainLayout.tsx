d7ab386543e797fa3fb8eb41df59fbe2314a47f9
khoatruong19 <thpthv.truonghuynhdangkhoa@gmail.com>
2024-08-20 22:52:34 +0700

    fix: overflow main layout

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -5,7 +5,9 @@ export const MainLayout = ({ children }: PropsWithChildren) => {
   return (
     <Layout container={false} className="bg-muted/40">
       <Sidebar />
-      <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">{children}</div>
+      <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14 max-h-screen overflow-y-auto">
+        {children}
+      </div>
     </Layout>
   );
 };
